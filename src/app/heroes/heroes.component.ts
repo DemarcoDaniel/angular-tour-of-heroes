c8d1745ae6d7da670c98157444c18d6825c6f2fb
Daniel Demarco <danieldemarco321@gmail.com>
2019-05-22 00:24:41 -0300

    Routing

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,31 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero';
+import {Component, OnInit} from '@angular/core';
+import {Hero} from '../hero';
 import {HeroService} from '../hero.service';
-import { Observable, of } from 'rxjs';
-
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-
 export class HeroesComponent implements OnInit {
-
-  selectedHero: Hero;
-
   heroes: Hero[];
-}
-  constructor(private heroservice: HeroService) {}
 
-  getHeroes(): Observable<Hero[]> {
-    return of(HEROES);
+  constructor(private heroService: HeroService) {
   }
-
 
   ngOnInit() {
-    getHeroes(): Observable<Hero[]> {
-    return of(HEROES);
-    }
+    this.getHeroes();
   }
 
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
+}
